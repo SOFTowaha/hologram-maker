751c8751c35308d4c01a2cbde119779fd107c0e2
Sergio López Ceballos <sergio.lceballos@alumnos.upm.es>
2018-08-09 19:38:57 +0200

    Create splash

--- app/src/main/java/com/sergioloc/hologram/Fragments/HomeFragment.java
+++ app/src/main/java/com/sergioloc/hologram/Fragments/HomeFragment.java
@@ -1,15 +1,12 @@
 package com.sergioloc.hologram.Fragments;
 
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
+
 import com.sergioloc.hologram.R;
 
 
@@ -34,7 +31,7 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.content_main, container, false);
+        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
 
---
+++ app/src/main/java/com/sergioloc/hologram/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.sergioloc.hologram;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, Auth.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000); // wait for 2 seconds
+    }
+}
