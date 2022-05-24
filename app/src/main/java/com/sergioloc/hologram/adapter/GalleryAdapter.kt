1dbeae1d515bd91fa99567f1b448e83ed9dcb582
Sergio <sergioloc2@gmail.com>
2022-05-24 13:25:48 +0200

    Minor fixes

--- app/src/main/java/com/sergioloc/hologram/adapter/GalleryAdapter.kt
+++ app/src/main/java/com/sergioloc/hologram/adapter/GalleryAdapter.kt
@@ -7,24 +7,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.sergioloc.hologram.R
+import com.sergioloc.hologram.utils.Converter
 import java.util.ArrayList
 
 class GalleryAdapter(var bitmaps: ArrayList<Bitmap>, var listener: OnHologramClickListener): RecyclerView.Adapter<GalleryAdapter.MyViewHolder>() {
 
+    private var margin = 0
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.card_image, parent, false)
+        margin = Converter.dpToPx(parent.context, 20f)
         return MyViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val bitmap = bitmaps[position]
+        //setMargins(holder.root, position)
 
         holder.ivImage.setImageBitmap(bitmap)
 
         holder.ivImage.setOnClickListener {
             listener.onHologramClick(bitmap, position)
         }
+    }
+
+    private fun setMargins(root: ConstraintLayout, position: Int) {
+        val rootParams = root.layoutParams as ViewGroup.MarginLayoutParams
+        // Left
+        if (position == 0 || position % 3 == 0)
+            rootParams.setMargins(margin, 0, margin/2, 0)
+        // Right
+        else if (position % 2 == 0)
+            rootParams.setMargins(margin/2, 0, margin, 0)
+        // Left
+        else
+            rootParams.setMargins(margin/2, 0, margin/2, 0)
+
+        root.layoutParams = rootParams
     }
 
     override fun getItemCount(): Int {
@@ -52,6 +73,7 @@ class GalleryAdapter(var bitmaps: ArrayList<Bitmap>, var listener: OnHologramCli
     }
 
     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var root: ConstraintLayout = itemView.findViewById(R.id.root)
         var ivImage: ImageView = itemView.findViewById(R.id.ivImage)
     }
 
