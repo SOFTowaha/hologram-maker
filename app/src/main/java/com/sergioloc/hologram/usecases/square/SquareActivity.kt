be0fb618d9991752c84d2fbda79c6472fda6b7d5
Sergio <sergioloc2@gmail.com>
2022-05-24 15:46:44 +0200

    New size & distance buttons

--- app/src/main/java/com/sergioloc/hologram/usecases/square/SquareActivity.kt
+++ app/src/main/java/com/sergioloc/hologram/usecases/square/SquareActivity.kt
@@ -1,26 +1,92 @@
 package com.sergioloc.hologram.usecases.square
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import com.needle.app.utils.extensions.gone
+import com.needle.app.utils.extensions.setOnSingleClickListener
+import com.needle.app.utils.extensions.visible
 import com.sergioloc.hologram.databinding.ActivitySquareBinding
 import com.sergioloc.hologram.utils.Session
-
 
 class SquareActivity: AppCompatActivity() {
 
     private lateinit var binding: ActivitySquareBinding
+    private var buttonsVisible = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySquareBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initView()
+        initButtons()
+
+        hideButtonsAfter(1.5f)
+    }
+
+    private fun initView() {
         Session.bitmap?.let {
             binding.ivTop.setImageBitmap(it)
             binding.ivBottom.setImageBitmap(it)
             binding.ivLeft.setImageBitmap(it)
             binding.ivRight.setImageBitmap(it)
         }
+    }
+
+    private fun initButtons() {
+        binding.root.setOnClickListener {
+            if (buttonsVisible)
+                hideButtons()
+            else {
+                showButtons()
+            }
+        }
+
+        // Size
+        binding.ivLessSize.setOnClickListener {
+
+        }
+        binding.ivMoreSize.setOnClickListener {
+
+        }
+
+        // Distance
+        binding.ivLessDistance.setOnClickListener {
+
+        }
+        binding.ivMoreDistance.setOnClickListener {
+
+        }
+    }
+
+    private fun showButtons() {
+        buttonsVisible = true
+        binding.ivLessSize.visible()
+        binding.ivMoreSize.visible()
+        binding.tvSize.visible()
+
+        binding.ivLessDistance.visible()
+        binding.ivMoreDistance.visible()
+        binding.tvDistance.visible()
+    }
+
+    private fun hideButtons() {
+        buttonsVisible = false
+        binding.ivLessSize.gone()
+        binding.ivMoreSize.gone()
+        binding.tvSize.gone()
+
+        binding.ivLessDistance.gone()
+        binding.ivMoreDistance.gone()
+        binding.tvDistance.gone()
+    }
+
+    private fun hideButtonsAfter(seconds: Float) {
+        Handler(Looper.getMainLooper()).postDelayed({
+            hideButtons()
+        }, (seconds * 1000).toLong())
     }
 
 }
