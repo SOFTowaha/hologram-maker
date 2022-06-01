3d2533acb6b65d5a14e342da50eece91eab3d9b3
Sergio <sergioloc2@gmail.com>
2022-06-01 10:05:51 +0200

    New application class

--- app/src/main/java/com/sergioloc/hologram/HologramApp.kt
+++ app/src/main/java/com/sergioloc/hologram/HologramApp.kt
@@ -1,4 +1,7 @@
 package com.sergioloc.hologram
 
-class HologramApp {
-}
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HologramApp: Application()
