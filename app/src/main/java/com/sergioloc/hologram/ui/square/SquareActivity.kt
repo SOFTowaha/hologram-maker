ffb3c2df2a821f3aa032296896a55ca482a7f12a
Sergio <sergioloc2@gmail.com>
2022-06-02 12:12:41 +0200

    New square seekbar

--- app/src/main/java/com/sergioloc/hologram/ui/square/SquareActivity.kt
+++ app/src/main/java/com/sergioloc/hologram/ui/square/SquareActivity.kt
@@ -1,23 +1,18 @@
 package com.sergioloc.hologram.ui.square
 
-import android.content.Context
-import android.content.SharedPreferences
 import android.graphics.Insets
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.DisplayMetrics
-import android.view.ViewGroup
+import android.view.View
 import android.view.WindowInsets
-import android.view.animation.AnimationUtils
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import com.needle.app.utils.extensions.gone
 import com.needle.app.utils.extensions.visible
-import com.sergioloc.hologram.R
 import com.sergioloc.hologram.databinding.ActivitySquareBinding
-import com.sergioloc.hologram.ui.dialogs.ConfirmationDialog
-import com.sergioloc.hologram.utils.Constants
 import com.sergioloc.hologram.utils.Converter
 import com.sergioloc.hologram.utils.Session
 import dagger.hilt.android.AndroidEntryPoint
@@ -26,51 +21,29 @@ import dagger.hilt.android.AndroidEntryPoint
 class SquareActivity: AppCompatActivity() {
 
     private lateinit var binding: ActivitySquareBinding
-    private lateinit var prefs: SharedPreferences
-    private var buttonsVisible = true
     private var width = 0f
     private var size = 100
-    private var distance = 0
-    private var initSize = 100
-    private var initDistance = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySquareBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initView()
         initVariables()
+        initView()
+        initListeners()
         initButtons()
 
         updateSize()
-        updateDistance()
-        hideButtonsAfter(1.5f)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            binding.seekBarSize.gone()
+        }, (1.5 * 1000).toLong())
     }
 
-    override fun onBackPressed() {
-        if (initSize != size || initDistance != distance) {
-            val dialog = ConfirmationDialog(this)
-            dialog.apply {
-                setTitle(getString(R.string.warning))
-                setMessage(getString(R.string.save_config))
-                setOnAcceptClickListener {
-                    with (prefs.edit()) {
-                        putInt(Constants.PREF_SIZE, size)
-                        putInt(Constants.PREF_DISTANCE, distance)
-                        apply()
-                    }
-                    super.onBackPressed()
-                }
-                setOnCancelClickListener {
-                    super.onBackPressed()
-                }
-                show()
-            }
-        }
-        else {
-            super.onBackPressed()
-        }
+    private fun initVariables() {
+        width = Converter.pxToDp(this, getScreenWidth().toFloat())
+        size = (width / 3).toInt()
     }
 
     private fun initView() {
@@ -80,131 +53,43 @@ class SquareActivity: AppCompatActivity() {
             binding.ivLeft.setImageBitmap(it)
             binding.ivRight.setImageBitmap(it)
         }
+
+        binding.seekBarSize.max = size
+        binding.seekBarSize.progress = size
+        binding.seekBarSize.incrementProgressBy(10)
     }
 
-    private fun initVariables() {
-        // Screen width
-        width = Converter.pxToDp(this, getScreenWidth().toFloat())
+    private fun initListeners() {
+        binding.seekBarSize.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+                size = if (progress < 10)
+                    10
+                else
+                    progress
+                updateSize()
+            }
 
-        // Shared Preferences
-        prefs = getSharedPreferences(Constants.PREFERENCES, Context.MODE_PRIVATE)
-        size = prefs.getInt(Constants.PREF_SIZE, 100)
-        initSize = prefs.getInt(Constants.PREF_SIZE, 100)
-        distance = prefs.getInt(Constants.PREF_DISTANCE, 0)
-        initDistance = prefs.getInt(Constants.PREF_DISTANCE, 0)
+            override fun onStartTrackingTouch(seek: SeekBar) { }
+
+            override fun onStopTrackingTouch(seek: SeekBar) { }
+        })
     }
 
     private fun initButtons() {
         binding.root.setOnClickListener {
-            if (buttonsVisible)
-                hideButtons()
+            if (binding.seekBarSize.visibility == View.VISIBLE)
+                binding.seekBarSize.gone()
             else {
-                showButtons()
+                binding.seekBarSize.visible()
             }
         }
-
-        // Size
-        binding.ivLessSize.setOnClickListener {
-            if (size > 10) {
-                size -= 10
-                updateSize()
-            }
-        }
-        binding.ivMoreSize.setOnClickListener {
-            if (3 * size + 2 * distance < width) {
-                size += 10
-                updateSize()
-            }
-        }
-
-        // Distance
-        binding.ivLessDistance.setOnClickListener {
-            if (distance > 0) {
-                distance -= 10
-                updateDistance()
-            }
-        }
-        binding.ivMoreDistance.setOnClickListener {
-            if (3 * size + 2 * distance < width) {
-                distance += 10
-                updateDistance()
-            }
-        }
-    }
-
-    private fun showButtons() {
-        buttonsVisible = true
-
-        binding.ivLessSize.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_in))
-        binding.ivMoreSize.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_in))
-        binding.tvSize.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_in))
-
-        binding.ivLessDistance.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_in))
-        binding.ivMoreDistance.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_in))
-        binding.tvDistance.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_in))
-
-        binding.ivLessSize.visible()
-        binding.ivMoreSize.visible()
-        binding.tvSize.visible()
-
-        binding.ivLessDistance.visible()
-        binding.ivMoreDistance.visible()
-        binding.tvDistance.visible()
-
-    }
-
-    private fun hideButtons() {
-        buttonsVisible = false
-
-        binding.ivLessSize.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_out))
-        binding.ivMoreSize.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_out))
-        binding.tvSize.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_out))
-
-        binding.ivLessDistance.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_out))
-        binding.ivMoreDistance.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_out))
-        binding.tvDistance.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_out))
-
-        binding.ivLessSize.gone()
-        binding.ivMoreSize.gone()
-        binding.tvSize.gone()
-
-        binding.ivLessDistance.gone()
-        binding.ivMoreDistance.gone()
-        binding.tvDistance.gone()
-    }
-
-    private fun hideButtonsAfter(seconds: Float) {
-        Handler(Looper.getMainLooper()).postDelayed({
-            hideButtons()
-        }, (seconds * 1000).toLong())
     }
 
     private fun updateSize() {
         binding.ivCross.layoutParams.height = Converter.dpToPx(this, size.toFloat())
         binding.ivCross.layoutParams.width = Converter.dpToPx(this, size.toFloat())
         binding.ivCross.requestLayout()
-    }
-
-    private fun updateDistance() {
-        // Top
-        var param = binding.ivTop.layoutParams as ViewGroup.MarginLayoutParams
-        param.setMargins(0,0,0, Converter.dpToPx(this, distance.toFloat()))
-        binding.ivTop.layoutParams = param
-
-        // Bottom
-        param = binding.ivBottom.layoutParams as ViewGroup.MarginLayoutParams
-        param.setMargins(0, Converter.dpToPx(this, distance.toFloat()),0,0)
-        binding.ivBottom.layoutParams = param
-
-        // Left
-        param = binding.ivLeft.layoutParams as ViewGroup.MarginLayoutParams
-        param.setMargins(0,0, Converter.dpToPx(this, distance.toFloat()),0)
-        binding.ivLeft.layoutParams = param
-
-        // Right
-        param = binding.ivRight.layoutParams as ViewGroup.MarginLayoutParams
-        param.setMargins(Converter.dpToPx(this, distance.toFloat()),0,0,0)
-        binding.ivRight.layoutParams = param
     }
 
     private fun getScreenWidth(): Int {
